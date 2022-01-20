f779363ff3652b17475700fa11f4c719d03ed542
Outshine <niefufeng@gmail.com>
2022-01-20 20:25:24 +0800

    inputBag::get default support array

--- InputBag.php
+++ InputBag.php
@@ -23,17 +23,17 @@ final class InputBag extends ParameterBag
     /**
      * Returns a scalar input value by name.
      *
-     * @param string|int|float|bool|null $default The default value if the input key does not exist
+     * @param string|int|float|bool|array|null $default The default value if the input key does not exist
      */
-    public function get(string $key, mixed $default = null): string|int|float|bool|null
+    public function get(string $key, mixed $default = null): string|int|float|bool|array|null
     {
-        if (null !== $default && !is_scalar($default) && !$default instanceof \Stringable) {
+        if (null !== $default && !is_scalar($default) && !is_array($default) && !$default instanceof \Stringable) {
             throw new \InvalidArgumentException(sprintf('Excepted a scalar value as a 2nd argument to "%s()", "%s" given.', __METHOD__, get_debug_type($default)));
         }
 
         $value = parent::get($key, $this);
 
-        if (null !== $value && $this !== $value && !is_scalar($value)) {
+        if (null !== $value && $this !== $value && !is_array($value) && !is_scalar($value)) {
             throw new BadRequestException(sprintf('Input value "%s" contains a non-scalar value.', $key));
         }
 
